637270431bd03ca5de0646ba499e0ea273af376a
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-04-19 16:37:17 +0630

    asd

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -1,13 +1,13 @@
+import Head from "../../components/head/Head";
 import Wbanner from "../../components/wish/wbanner/Wbanner";
 import Wtable from "../../components/wtable/Wtable";
 
 export default function Wishlist() {
   return (
     <>
+      <Head />
       <Wbanner />
-      <div>
-        <Wtable />
-      </div>
+      <Wtable />
     </>
   );
 }
