a9856d8949bfd053af36b029f4508d52f7e9cc2f
chue wathan kyaw <chuewathan.k24@gmail.com>
2022-04-20 10:02:28 +0630

    kkk

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Blog from "./pages/blog/Blog";
 import Wishlist from "./pages/wishlist/Wishlist";
 
 function App() {
-  return <Wishlist />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Wishlist" element={<Wishlist />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
