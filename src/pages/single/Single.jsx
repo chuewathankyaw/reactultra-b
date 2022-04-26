b6c1a59547778c2e880069e2b1c944a113e991f8
Mg Thung <thunghtike7@gmail.com>
2022-04-26 17:26:33 +0630

    add single2

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Detail from "../../components/single/Detail";
+import Head from "../../components/head/Head";
+import Footer from "../../components/footer/Footer";
+import Homenewarrival from "../../components/homecomponents/Homenewarrival";
+const Single = () => {
+  return (
+    <>
+      <Head />
+      <Detail />
+      <Homenewarrival />
+      <Footer />
+    </>
+  );
+};
+
+export default Single;
